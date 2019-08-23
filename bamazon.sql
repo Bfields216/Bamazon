634ed0a02f48e63b099a5510fc39c30bba1fb154
Brandon Fields <brandonfields10@icloud.com>
2019-08-22 20:35:43 -0400

    got the file to connect and show the table, prompt works, just working on adding more

--- bamazon.sql
+++ bamazon.sql
@@ -22,4 +22,4 @@ VALUES (001, "baseball caps", "baseball", 25.99, 30),
         (404, "cavs shorts", "basketball", 34.85, 15),
         (515, "Mayfield", "football", 60.99, 2),
         (646, "gloves", "hockey", 40.00, 11),
-        (777, "Ken Griffey", "baseball", 16.17, 4),
+        (777, "Ken Griffey", "baseball", 16.17, 4)
---
+++ bamazonCustomer.js
@@ -0,0 +1,79 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "mynewpassword",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Welcome to Bamazon, ");
+    queryeverything();
+});
+
+function queryeverything() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log("\n" + res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity)
+        };
+
+
+
+
+        firstPrompt();
+    });
+
+    function firstPrompt() {
+        inquirer.prompt([
+            {
+                name: "ID",
+                type: "input",
+                message: "Please enter Item ID you like to get.",
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many items do you want?",
+            },
+        ]).then(function (answers) {
+            var quantityNeed = answers.quantity;
+            var IDrequested = answers.ID;
+            purchase(IDrequested, quantityNeed);
+            var query = "SELECT item_Id, product_name, price, stock_quantity FROM products WHERE ?";
+
+            connection.query(query, { item_Id: answer.productID }, function (err, res) {
+                var actualAmount = res[0].stock_quantity
+                if (unitamt > actualAmount) {
+                    console.log("\nINSUFFICIENT QUANTITY: Im sorry, there is only " + actualAmount + " of that product currently in stock.\n");
+                    runSearch();
+                } else {
+                    var queryUpdate = "UPDATE products SET ? WHERE ?";
+                    connection.query(queryUpdate, [{ stock_quantity: actualAmount - unitamt }, { item_Id: res[0].item_Id }], function (err, response) { });
+                 
+                    console.log("The total cost of your purchase today comes to $" + (unitamt * res[0].price) + "\n");
+                    inquirer
+                        .prompt([
+                            {
+                                type: 'confirm',
+                                name: 'confirm',
+                                message: "Would you like to confirm this purchase? "
+                            }
+                        ]).then(answer => {
+                            if (answer === true) {
+                                console.log("You should recive an email shortly confirming your purchase of " + unitamt + " " + res[0].product_name + " for $" + (unitamt * res[0].price) + "\n")
+                            }
+                            runSearch();
+                        })
+                }
+            })
+        })
+    }
+}
+    
+
+
