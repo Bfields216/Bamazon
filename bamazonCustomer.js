ce9c1aae510b5ab0fccee0e26b3cbd4833f38c47
Brandon Fields <brandonfields10@icloud.com>
2019-08-23 16:23:13 -0400

    got code working for customer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,16 +22,13 @@ function queryeverything() {
             console.log("\n" + res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity)
         };
 
-
-
-
         firstPrompt();
     });
 
     function firstPrompt() {
         inquirer.prompt([
             {
-                name: "ID",
+                name: "pdID",
                 type: "input",
                 message: "Please enter Item ID you like to get.",
             },
@@ -42,20 +39,19 @@ function queryeverything() {
             },
         ]).then(function (answers) {
             var quantityNeed = answers.quantity;
-            var IDrequested = answers.ID;
-            purchase(IDrequested, quantityNeed);
+         
             var query = "SELECT item_Id, product_name, price, stock_quantity FROM products WHERE ?";
 
-            connection.query(query, { item_Id: answer.productID }, function (err, res) {
+            connection.query(query, { item_Id: answers.pdID}, function (err, res) {
                 var actualAmount = res[0].stock_quantity
-                if (unitamt > actualAmount) {
+                if (quantityNeed > actualAmount) {
                     console.log("\nINSUFFICIENT QUANTITY: Im sorry, there is only " + actualAmount + " of that product currently in stock.\n");
-                    runSearch();
+                    firstPrompt();
                 } else {
                     var queryUpdate = "UPDATE products SET ? WHERE ?";
-                    connection.query(queryUpdate, [{ stock_quantity: actualAmount - unitamt }, { item_Id: res[0].item_Id }], function (err, response) { });
+                    connection.query(queryUpdate, [{ stock_quantity: actualAmount - quantityNeed }, { item_Id: res[0].item_Id }], function (err, response) { });
                  
-                    console.log("The total cost of your purchase today comes to $" + (unitamt * res[0].price) + "\n");
+                    console.log("The total cost of your purchase today comes to $" + (quantityNeed * res[0].price) + "\n");
                     inquirer
                         .prompt([
                             {
@@ -65,9 +61,9 @@ function queryeverything() {
                             }
                         ]).then(answer => {
                             if (answer === true) {
-                                console.log("You should recive an email shortly confirming your purchase of " + unitamt + " " + res[0].product_name + " for $" + (unitamt * res[0].price) + "\n")
+                                console.log("You should recive an email shortly confirming your purchase of " + quantityNeed + " " + res[0].product_name + " for $" + (quantityNeed * res[0].price) + "\n")
                             }
-                            runSearch();
+                            firstPrompt();
                         })
                 }
             })
